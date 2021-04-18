0ec9203279454450de5d24e06330d701ab0577ab
aravind852 <65849381+aravind852@users.noreply.github.com>
2021-04-18 22:17:30 +0530

    Add files via upload

---
+++ sample/src/sample/input.java
@@ -0,0 +1,11 @@
+package exercise3;
+import java.util.*;
+public class input {
+
+	public static void main(String args[]) {
+		Scanner sc =new Scanner(System.in);
+		System.out.println("reading input");
+		String a =sc.nextLine();
+		System.out.println("input is "+a);
+	}
+}
